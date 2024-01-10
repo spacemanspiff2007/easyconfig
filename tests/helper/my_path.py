41d803ee0872b9e3d66b2fa61dc30b5cfe5eb201
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-01-10 08:08:04 +0100

    3.12 tests

--- tests/helper/my_path.py
+++ tests/helper/my_path.py
@@ -1,34 +1,43 @@
 from io import StringIO, TextIOWrapper
 from pathlib import Path as _Path
 from typing import Optional
-
+from sys import version_info
 
 _path_type = type(_Path())
 
 
 class Path(_path_type):
-    _flavour = _path_type._flavour
+    if version_info < (3, 12):
+        _flavour = _path_type._flavour
 
-    def __init__(self, *args, does_exist: bool = True, initial_value: Optional[str] = None, **kwargs):
-        super().__init__()
+        def __init__(self, *args, does_exist: bool = True, initial_value: Optional[str] = None, **kwargs):
+            super().__init__()
 
-        if hasattr(Path, '_from_parts'):
-            Path._from_parts(args)
-        else:
-            # Pathlib init
-            drv, root, parts = self._parse_args(args)
-            self._drv = drv
-            self._root = root
-            self._parts = parts
-            self._init()
+            if hasattr(Path, '_from_parts'):
+                Path._from_parts(args)
+            else:
+                # Pathlib init
+                drv, root, parts = self._parse_args(args)
+                self._drv = drv
+                self._root = root
+                self._parts = parts
+                self._init()
 
-        # Own Path implementation
-        self.does_exist: bool = does_exist
-        self.contents = None
-        self._create_buffer(initial_value)
+            # Own Path implementation
+            self.does_exist: bool = does_exist
+            self.contents = None
+            self._create_buffer(initial_value)
 
-    def __new__(cls, *args, **kwargs):
-        return super().__new__(cls, *args)
+        def __new__(cls, *args, **kwargs):
+            return super().__new__(cls, *args)
+
+    else:
+        def __init__(self, *args, does_exist: bool = True, initial_value: Optional[str] = None, **kwargs):
+            super().__init__()
+            # Own Path implementation
+            self.does_exist: bool = does_exist
+            self.contents = None
+            self._create_buffer(initial_value)
 
     def _create_buffer(self, initial_value: Optional[str] = None):
         self.contents = StringIO(initial_value)
